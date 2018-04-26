550688879aaac171166e64efbc1e202904a54687
github username <github email address>
2018-04-26 15:46:16 -0400

    push before editing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def
+    @user = User.find(params[:id])
+
+    @user.update(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: {errors: @note.errors.full_messages}, status: 422
+    end
+  end
+
+  private
+    # def 
+    #   params.permit()
+    # end
+
 end
